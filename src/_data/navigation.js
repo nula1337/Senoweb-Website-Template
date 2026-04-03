a64a72717d21108f1537ded95f8ce71b8f807440
Ondřej Senohrábek <ondrej.senohrabek@gmail.com>
2026-04-03 12:02:42 +0200

    fix: navigation trailing slashes

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -1,11 +1,11 @@
 export default [
     { title: "Domů", url: "/" },
-    { title: "O nás", url: "/o-nas" },
+    { title: "O nás", url: "/o-nas/" },
     {
-        title: "Služby", url: "/sluzby", subitems: [
-            { title: "Tvorba webových stránek", url: "/sluzby/tvorba-webovych-stranek" },
-            { title: "Webdesign", url: "/sluzby/webdesign" }
+        title: "Služby", url: "/sluzby/", subitems: [
+            { title: "Tvorba webových stránek", url: "/sluzby/tvorba-webovych-stranek/" },
+            { title: "Webdesign", url: "/sluzby/webdesign/" }
         ]
     },
-    { title: "Kontakt", url: "/kontakt" }
+    { title: "Kontakt", url: "/kontakt/" }
 ]
